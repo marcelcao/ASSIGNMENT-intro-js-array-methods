ad8b0f5b1e776ab10b2b35724e0586d5c23f6d71
Mara Caoile <mara.caoile@gmail.com>
2023-04-26 21:37:01 -0500

    updated code per code along video

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,13 @@ import { renderToDom } from "../utils/renderToDom.js";
 // Reusable function to get the cards on the DOM
 // .forEach()
 const renderCards = (array) => {
-  let refStuff = "<h1 class='text-white'>Cards Go Here!</h1>";
+  let refStuff = "";
+  array.forEach((item) => {
+     refStuff += card(item);
+  });
+  
+  
+  
   renderToDom("#cards", refStuff);
 }
 
@@ -14,7 +20,16 @@ const renderCards = (array) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+   //destructuring - as in similar to splitting a string. (.split) splits an array//
+   const [, id] = event.target.id.split('--');
+    // converts id string  to a number
+   const index = referenceList.findIndex(item => item.id === Number(id))
+    //boolean statement checking if the card, which is identifgied by the index function using its index position, is the cart or not in the cart
+   referenceList[index].inCart = !referenceList[index].inCart
+   //this is the variable that gets called out in order to display how much is on the cart. there's no paramenters assigned to it. but if you scroll down, there's a bunch of other functions that are calling back to it
+   cartTotal();
+   //the function below just keeps rendering the cards again to the DOM
+   renderCards(referenceList);
   }
 }
 
@@ -22,23 +37,33 @@ const toggleCart = (event) => {
 // .filter()
 const search = (event) => {
   const eventLC = event.target.value.toLowerCase();
-  console.log(eventLC)
+  const searchResult  = referenceList.filter(item => 
+    item.title.toLowerCase().includes(eventLC)||
+    item.author.toLowerCase().includes(eventLC) ||
+    item.description.toLowerCase().includes(eventLC)
+    )
+    renderCards(searchResult);
+    // seachresult is passed because it's the code block that executes the function that enables you to pass values through the search bar
+    // const search it the entire code function that enables you to search
 }
 
 // BUTTON FILTER
 // .filter() & .reduce() &.sort() - chaining
 const buttonFilter = (event) => {
   if(event.target.id.includes('free')) {
-    console.log('FREE')
+    const free = referenceList.filter(item => item.price <= 0);
+    renderCards(free);
   }
   if(event.target.id.includes('cartFilter')) {
-    console.log('cartFilter')
+    const wishlist = referenceList.filter(wantItem => wantItem.inCart);
+    renderCards(wishlist);
   }
   if(event.target.id.includes('books')) {
-    console.log('books!')
+    const books = referenceList.filter(item => item.type.toLowerCase() === 'book');
+    renderCards(books);
   }
   if(event.target.id.includes('clearFilter')) {
-    console.log('clearFilter')
+    renderCards(referenceList);
   }
   if(event.target.id.includes('productList')) {
     let table = `<table class="table table-dark table-striped" style="width: 600px">
@@ -66,14 +91,34 @@ const buttonFilter = (event) => {
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
+  // this function gives us all the items in the cart
+  const cart = referenceList.filter(item => item.inCart);
+  //a,b = the parameters passed
+  // a is the constant initial value
+  //b is the value it's looking at when it's looping through
+  //a + b.price = .reduce is about adding values. .price is the key we're targetubg
+  // 0 is the default/initial value
+  const total = cart.reduce((a, b) => a + b.price, 0);
+  //.some method just sees if theres any instances of the condition passed (it's a boolean type of function - answer true or false)
+  const freeItem = cart.some(item => item.price <= 0);
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
-}
+
+  if (freeItem) {
+    document.querySelector('#includes-free').innerHTML = 'INCLUDES FREE ITEMS'
+  } else {
+    document.querySelector('#includes-free').innerHTML = ' '
+  }
+  }
+
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
 const productList = () => {
-  return [{ title: "SAMPLE TITLE", price: 45.00, type: "SAMPLE TYPE" }]
+  return referenceList.map(item => ({ 
+    title: item.title, 
+    price: item.price, 
+    type: item.type
+  }));
 }
 
 
